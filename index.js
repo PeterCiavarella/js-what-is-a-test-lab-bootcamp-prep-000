656468a68bbf9524abfa0b84aa26758c404310b6
Peter Ciavarella <peterciavarella@gmail.com>
2017-10-03 23:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var name = "Susan"
 var height = '39'
 var message = ` ${name} is ${height} inches tall`
 
-console.log("message")
+console.log(message).inner
 
 module.exports = {name, height, message}
